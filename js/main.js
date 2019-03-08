623f72bd15015231fefe4ae79d8bed43c1e4ee0e
Eslam Shams <44995250+EslamShams@users.noreply.github.com>
2019-03-08 21:22:21 +0200

    My Second Project

---
+++ js/main.js
@@ -0,0 +1,339 @@
+const topImg_1 = document.getElementById('top1'),
+      topImg_2 = document.getElementById('top2'),
+      topImg_3 = document.getElementById('top3'),
+      secondRow_1 = document.getElementById('secondrow1'),
+      secondRow_2 = document.getElementById('secondrow2'),
+      secondRow_3 = document.getElementById('secondrow3'),
+      thirdRow_1 = document.getElementById('thirdrow1'),
+      thirdRow_2 = document.getElementById('thirdrow2'),
+      thirdRow_3 = document.getElementById('thirdrow3'),
+      lastRow_1 = document.getElementById('lastrow1'),
+      lastRow_2 = document.getElementById('lastrow2'),
+      lastRow_3 = document.getElementById('lastrow3');
+///////////////////////////My Images/////////////////////////////////////////////
+
+//////////////////////// My Modals /////////////////////////////////////////////
+const boxtop1 = document.getElementById('modal1'),
+      boxtop2 = document.getElementById('modal2'),
+      boxtop3 = document.getElementById('modal3'),
+
+      secondBox1 = document.getElementById('modal4'),
+      secondBox2 = document.getElementById('modal5'),
+      secondBox3 = document.getElementById('modal6'),
+
+      thirdBox1 = document.getElementById('modal7'),
+      thirdBox2 = document.getElementById('modal8'),
+      thirdBox3 = document.getElementById('modal9'),
+
+      lastBox1 = document.getElementById('modal10'),
+      lastBox2 = document.getElementById('modal11'),
+      lastBox3 = document.getElementById('modal12');
+      
+///////////////span///////////////////////////////////////////////////
+
+const times1 = document.getElementById('one').onclick = () => {
+    if (boxtop1.style.display = 'block') {
+        boxtop1.style.display = 'none';
+    }
+}
+
+////////////////////////////////////////////////////////////////
+const times2 = document.getElementById('two').onclick = () => {
+    if (boxtop2.style.display = 'block') {
+        boxtop2.style.display = 'none';
+    }
+}
+////////////////////////////////////////////////////////////////////// 
+const times3 = document.getElementById('three').onclick = () => {
+    if (boxtop3.style.display = 'block') {
+        boxtop3.style.display = 'none';
+    }
+}
+
+/////////////////////////////////////////////////////////////////////
+const times4 = document.getElementById('four').onclick = () => {
+    if (secondBox1.style.display = 'block') {
+        secondBox1.style.display = 'none';
+    }
+}
+
+////////////////////////////////////////////////////////////////////////
+ 
+const times5 = document.getElementById('five').onclick = () => {
+    if (secondBox2.style.display = 'block') {
+        secondBox2.style.display = 'none';
+    }
+}
+
+///////////////////////////////////////////////////////////////////
+const times6 = document.getElementById('six').onclick = () => {
+    if (secondBox3.style.display = 'block') {
+        secondBox3.style.display = 'none';
+    }
+}
+
+
+
+//////////////////////////////////////////////////////////////////
+const times7 = document.getElementById('seven').onclick = () => {
+    if (thirdBox1.style.display = 'block') {
+        thirdBox1.style.display = 'none';
+    }
+}
+
+////////////////////////////////////////////////////////////////////////
+const times8 = document.getElementById('eight').onclick = () => {
+    if (thirdBox2.style.display = 'block') {
+        thirdBox2.style.display = 'none';
+    }
+}
+
+/////////////////////////////////////////////////////////////////////////
+const times9 = document.getElementById('nine').onclick = () => {
+    if (thirdBox3.style.display = 'block') {
+        thirdBox3.style.display = 'none';
+    }
+}
+///////////////////////////////////////////////////////////////////
+const times10 = document.getElementById('ten').onclick = () => {
+    if (lastBox1.style.display = 'block') {
+        lastBox1.style.display = 'none';
+    }
+}
+////////////////////////////////////////////////////////////////////
+const times11 = document.getElementById('eleven').onclick = () => {
+    if (lastBox2.style.display = 'block') {
+        lastBox2.style.display = 'none';
+    }
+}
+
+////////////////////////////////////////////////////////////////////////
+const times12 = document.getElementById('twelve').onclick = () => {
+    if (lastBox3.style.display = 'block') {
+        lastBox3.style.display = 'none';
+    }
+}
+
+////////////////////////////////////////////////////////////////////// 
+topImg_1.onclick = ()=> {
+    if(boxtop1.style.display = 'none') {
+        boxtop1.style.display = 'block';
+        boxtop2.style.display = 'none';
+        boxtop3.style.display = 'none';
+        secondBox1.style.display = 'none';
+        secondBox2.style.display = 'none';
+        secondBox3.style.display = 'none';
+        thirdBox1.style.display = 'none';
+        thirdBox2.style.display = 'none';
+        thirdBox3.style.display = 'none';
+        lastBox1.style.display = 'none';
+        lastBox2.style.display = 'none';
+        lastBox3.style.display = 'none';
+        window.scrollTo(30,0);
+        
+    }
+}
+topImg_2.onclick = ()=> {
+    if(boxtop2.style.display = 'none') {
+        boxtop2.style.display = 'block';
+        boxtop1.style.display = 'none';
+        boxtop3.style.display = 'none';
+        secondBox1.style.display = 'none';
+        secondBox2.style.display = 'none';
+        secondBox3.style.display = 'none';
+        thirdBox1.style.display = 'none';
+        thirdBox2.style.display = 'none';
+        thirdBox3.style.display = 'none';
+        lastBox1.style.display = 'none';
+        lastBox2.style.display = 'none';
+        lastBox3.style.display = 'none';
+        window.scrollTo(30,0);    
+    }
+}
+topImg_3.onclick = ()=> {
+    if(boxtop3.style.display = 'none') {
+        boxtop3.style.display = 'block';
+        boxtop1.style.display = 'none';
+        boxtop2.style.display = 'none';
+        secondBox1.style.display = 'none';
+        secondBox2.style.display = 'none';
+        secondBox3.style.display = 'none';
+        thirdBox1.style.display = 'none';
+        thirdBox2.style.display = 'none';
+        thirdBox3.style.display = 'none';
+        lastBox1.style.display = 'none';
+        lastBox2.style.display = 'none';
+        lastBox3.style.display = 'none';
+        window.scrollTo(30,0);
+        
+    }
+}
+secondRow_1.onclick = ()=> {
+    if(secondBox1.style.display = 'none') {
+        secondBox1.style.display = 'block';
+        boxtop1.style.display = 'none';
+        boxtop2.style.display = 'none';
+        boxtop3.style.display = 'none';
+        secondBox2.style.display = 'none';
+        secondBox3.style.display = 'none';
+        thirdBox1.style.display = 'none';
+        thirdBox2.style.display = 'none';
+        thirdBox3.style.display = 'none';
+        lastBox1.style.display = 'none';
+        lastBox2.style.display = 'none';
+        lastBox3.style.display = 'none';
+        window.scrollTo(30,0);
+        
+    }
+}
+secondRow_2.onclick = ()=> {
+    if(secondBox2.style.display = 'none') {
+        secondBox2.style.display = 'block';
+        boxtop1.style.display = 'none';
+        boxtop2.style.display = 'none';
+        boxtop3.style.display = 'none';
+        secondBox1.style.display = 'none';
+        secondBox3.style.display = 'none';
+        thirdBox1.style.display = 'none';
+        thirdBox2.style.display = 'none';
+        thirdBox3.style.display = 'none';
+        lastBox1.style.display = 'none';
+        lastBox2.style.display = 'none';
+        lastBox3.style.display = 'none';
+        window.scrollTo(30,0);
+        
+        
+    }
+}
+secondRow_3.onclick = ()=> {
+    if(secondBox3.style.display = 'none') {
+        secondBox3.style.display = 'block';
+        boxtop1.style.display = 'none';
+        boxtop2.style.display = 'none';
+        boxtop3.style.display = 'none';
+        secondBox1.style.display = 'none';
+        secondBox2.style.display = 'none';
+        thirdBox1.style.display = 'none';
+        thirdBox2.style.display = 'none';
+        thirdBox3.style.display = 'none';
+        lastBox1.style.display = 'none';
+        lastBox2.style.display = 'none';
+        lastBox3.style.display = 'none';
+        window.scrollTo(30,0);
+        
+        
+    }
+}
+
+thirdRow_1.onclick = ()=> {
+    if(thirdBox1.style.display = 'none') {
+        thirdBox1.style.display = 'block';
+        boxtop1.style.display = 'none';
+        boxtop2.style.display = 'none';
+        boxtop3.style.display = 'none';
+        secondBox1.style.display = 'none';
+        secondBox2.style.display = 'none';
+        secondBox3.style.display = 'none';
+        thirdBox2.style.display = 'none';
+        thirdBox3.style.display = 'none';
+        lastBox1.style.display = 'none';
+        lastBox2.style.display = 'none';
+        lastBox3.style.display = 'none';
+        window.scrollTo(30,0);
+        
+        
+    }
+}
+thirdRow_2.onclick = ()=> {
+    if(thirdBox2.style.display = 'none') {
+        thirdBox2.style.display = 'block';
+        boxtop1.style.display = 'none';
+        boxtop2.style.display = 'none';
+        boxtop3.style.display = 'none';
+        secondBox1.style.display = 'none';
+        secondBox2.style.display = 'none';
+        secondBox3.style.display = 'none';
+        thirdBox1.style.display = 'none';
+        thirdBox3.style.display = 'none';
+        lastBox1.style.display = 'none';
+        lastBox2.style.display = 'none';
+        lastBox3.style.display = 'none';
+        window.scrollTo(30,0);
+        
+        
+    }
+}
+thirdRow_3.onclick = ()=> {
+    if(thirdBox3.style.display = 'none') {
+        thirdBox3.style.display = 'block';
+        boxtop1.style.display = 'none';
+        boxtop2.style.display = 'none';
+        boxtop3.style.display = 'none';
+        secondBox1.style.display = 'none';
+        secondBox2.style.display = 'none';
+        secondBox3.style.display = 'none';
+        thirdBox1.style.display = 'none';
+        thirdBox2.style.display = 'none';
+        lastBox1.style.display = 'none';
+        lastBox2.style.display = 'none';
+        lastBox3.style.display = 'none';
+        window.scrollTo(30,0);
+        
+    }
+}         
+lastRow_1.onclick = ()=> {
+    if(lastBox1.style.display = 'none') {
+        lastBox1.style.display = 'block';
+        boxtop1.style.display = 'none';
+        boxtop2.style.display = 'none';
+        boxtop3.style.display = 'none';
+        secondBox1.style.display = 'none';
+        secondBox2.style.display = 'none';
+        secondBox3.style.display = 'none';
+        thirdBox1.style.display = 'none';
+        thirdBox2.style.display = 'none';
+        thirdBox3.style.display = 'none';
+        secondBox1.style.display = 'none';
+        lastBox2.style.display = 'none';
+        lastBox3.style.display = 'none';
+        window.scrollTo(30,0); 
+    }
+}
+lastRow_2.onclick = ()=> {
+    if(lastBox2.style.display = 'none') {
+        lastBox2.style.display = 'block';
+        boxtop1.style.display = 'none';
+        boxtop2.style.display = 'none';
+        boxtop3.style.display = 'none';
+        secondBox2.style.display = 'none';
+        secondBox1.style.display = 'none';
+        secondBox3.style.display = 'none';
+        thirdBox1.style.display = 'none';
+        thirdBox2.style.display = 'none';
+        thirdBox3.style.display = 'none';
+        lastBox1.style.display = 'none';
+        lastBox3.style.display = 'none';
+        window.scrollTo(30,1250);
+        
+        
+    }
+} 
+
+lastRow_3.onclick = ()=> {
+    if(lastBox3.style.display = 'none') {
+        lastBox3.style.display = 'block';
+        boxtop1.style.display = 'none';
+        boxtop2.style.display = 'none';
+        boxtop3.style.display = 'none';
+        secondBox2.style.display = 'none';
+        secondBox1.style.display = 'none';
+        secondBox3.style.display = 'none';
+        thirdBox1.style.display = 'none';
+        thirdBox2.style.display = 'none';
+        thirdBox3.style.display = 'none';
+        lastBox1.style.display = 'none';
+        lastBox2.style.display = 'none';
+        window.scrollTo(30,1250); 
+    }
+}
+                                
